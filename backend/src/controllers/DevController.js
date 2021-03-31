e9c6d68b05cae84b214ea20477dea39fcda4cffd
Guilherme Aguiar <kadeguilherme@gmail.com>
2021-03-31 10:57:06 -0300

    Pegando as infromações importantes dentro do perfil

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,15 +1,21 @@
 const axios = require('axios');
+const Dev = require('../models/Dev');
 
 module.exports = {
     async store(req,res) { 
         const { username } = req.body;
 
         const response = await axios.get(`https://api.github.com/users/${username}`);
-         
-        //As informações no axios esta dentro do data
-        console.log(response.data);
 
+        const {name, bio, avatar_url: avatar } = response.data;
+        
+       const dev = await Dev.create({
+            name,
+            user: username,
+            bio,
+            avatar
+        })
 
-        return res.json(response.data);
+        return res.json(dev);
     }
 }
