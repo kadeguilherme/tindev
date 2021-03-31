6780072f16a7f123c2bd5b4d8011b8214d5ff64d
Guilherme Aguiar <kadeguilherme@gmail.com>
2021-03-31 10:39:42 -0300

    Consertando o console.log para não dar erro no insomnia

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,8 @@
 module.exports = {
     store(req,res) { 
-        console.log(json.body)
+        console.log(req.body.username);
+        //Poder ser o debaixo usando a desestruturação
+        const { username } = req.body;
         return res.json({ok: true });
     }
 }
