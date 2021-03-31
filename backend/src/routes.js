870662515d4c546eb03112bc96afe796693a8f61
Guilherme Aguiar <kadeguilherme@gmail.com>
2021-03-31 13:01:26 -0300

    Fazendo uma filtragem para mostra os que nao tem like e dislike

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ const DisLikeController = require('./controllers/DisLikeController');
 
 const routes = express.Router();
 
+routes.get('/devs', DevController.index);
 routes.post('/devs',DevController.store);
 routes.post('/devs/:devId/likes',LikeController.store);
 routes.post('/devs/:devId/dislikes',DisLikeController.store);
