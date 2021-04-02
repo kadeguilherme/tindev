c1d97e7da1f96784c05bb43f4c31bc5669d5b2ac
Guilherme Aguiar <kadeguilherme@gmail.com>
2021-04-02 10:16:37 -0300

    Usando useState para trabalhar com varivel dentro do form

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo.svg';
 import './login.css';
 
 export default function Login(){
+const [username, setUsername] = useState('');
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        console.log(username);
+    }
+
     return (
    <div className = "login-container">
        
         <img src = {logo} alt = 'Tindev'/>
-       <form>
+       <form onSubmit = {handleSubmit}>
         <input 
         placeholder = "Digite seu usuário do GitHub"
+        value = {username}
+        onChange = {e => setUsername(e.target.value)
+        }
         />
         <button>
            Enviar
